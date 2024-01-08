a40156866914fba35ce190a2110dafd7d85256f6
Michaiah Bos <michaiahbos@yahoo.com>
2024-01-08 12:51:46 -0600

    done

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -1,30 +1,31 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 
 import { data } from "./data";
 
 import FamilyTree from "./components/FamilyTree";
 import "./styles.scss";
+import Families from "./components/Families";
+
+export const FamilyContext = createContext();
 
 export default function App() {
-  const [families] = useState(data);
-  const [activeFamily, setActiveFamily] = useState(families[0]);
+  const [families] = useState(data); // initalizes the state variable families with the data array.
+  const [activeFamily, setActiveFamily] = useState(families[0]); // initalizes the state variable 'activeFamily' withe the first family in the array.
 
   return (
     <div className="App">
-      <section className="header">
-        <h1>Family Trees</h1>
-        {families.map(family => (
-          <button
-            className={`family-button ${family.familyName ===
-              activeFamily.familyName && "active"}`}
-            key={family.familyName}
-            onClick={() => setActiveFamily(family)}
-          >
-            {family.familyName}
-          </button>
-        ))}
-      </section>
-      {activeFamily && <FamilyTree family={activeFamily} />}
+      <FamilyContext.Provider
+        value={{
+          activeFamily: activeFamily,
+          setActiveFamily: setActiveFamily,
+          families: families,
+        }}
+      >
+        <Families />
+        {activeFamily && <FamilyTree />}
+      </FamilyContext.Provider>
     </div>
   );
 }
+
+// entry point for the application
